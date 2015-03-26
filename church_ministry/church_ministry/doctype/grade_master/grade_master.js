0bed6cb806c3023c9499b4727c56bba7b1e66599
gangadhar <email.kadam@gmail.com>
2015-03-26 12:52:44 +0530

    Deleted files manually

--- church_ministry/church_ministry/doctype/grade_master/grade_master.js
+++
@@ -1,9 +0,0 @@
-
-frappe.ui.form.on("Grade Master", "to_score", function(frm,doc) {
-    if(frm.doc.from_score){
-   		if(frm.doc.from_score>=frm.doc.to_score){
-   			msgprint("To-Score should be greater than From-Score..! ");
-        	throw "Please Enter valid score!"
-   		}
-   }
-});
